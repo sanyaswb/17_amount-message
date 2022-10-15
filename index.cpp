58872e83e68b7937f32e25e6f3aa0a7544e4c05e
sanyaswb <sanyaswb@gmail.com>
2022-10-15 21:15:00 +0300

    completed amount message

--- index.cpp
+++ index.cpp
@@ -3,5 +3,13 @@
 #include <cmath>
 
 std::string message(int (&numbers)[5]) {
-	return " ";
+	int sum = 0;
+
+	for (int i = 0; i < 5; ++i) {
+		sum += numbers[i];
+	}
+
+	std::string sumStr = std::to_string(sum);
+
+	return "amount: " + sumStr;
 }
