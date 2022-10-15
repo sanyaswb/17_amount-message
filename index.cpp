7117236e4825211b36bf182b73b5b945653b22d8
John Doe <SlySpellar@gmail.com>
2022-10-15 09:29:07 +0300

    removed solution

--- index.cpp
+++ index.cpp
@@ -3,11 +3,5 @@
 #include <cmath>
 
 std::string message(int (&numbers)[5]) {
-	int sum = 0;
-
-	for (int n: numbers) {
-		sum += n;
-	}
-
-	return "amount: " + std::to_string(sum);
+	return " ";
 }
